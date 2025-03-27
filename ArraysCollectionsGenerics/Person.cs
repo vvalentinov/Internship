f1675b892f4c17450272e1989d3630d6c25282ed
Valentin Valentinov <95280488+vvalentinov@users.noreply.github.com>
2025-03-27 08:28:00 +0200

    Create Person class

---
+++ ArraysCollectionsGenerics/Person.cs
@@ -0,0 +1,25 @@
+﻿namespace ArraysCollectionsGenerics;
+
+using static System.Environment;
+
+public class Person : IEntity
+{
+    public Person(
+        int id,
+        string name,
+        int age)
+    {
+        Id = id;
+        Name = name;
+        Age = age;
+    }
+
+    public int Id { get; init; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public int Age { get; set; }
+
+    public override string ToString()
+        => $"{nameof(Id)}: {Id}{NewLine}{nameof(Name)}: {Name}{NewLine}{nameof(Age)}: {Age}";
+}
