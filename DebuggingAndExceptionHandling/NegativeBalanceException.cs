dc33608c70212fbd5a32e130c8a8829ebb341b3a
Valentin Valentinov <95280488+vvalentinov@users.noreply.github.com>
2025-03-27 08:41:19 +0200

    Create custom NegativeBalanceException

---
+++ DebuggingAndExceptionHandling/NegativeBalanceException.cs
@@ -0,0 +1,18 @@
+﻿namespace DebuggingAndExceptionHandling;
+
+public class NegativeBalanceException : Exception
+{
+    public NegativeBalanceException()
+    {
+    }
+
+    public NegativeBalanceException(string message)
+        : base(message)
+    {
+    }
+
+    public NegativeBalanceException(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+}
