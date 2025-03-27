35ebdeb4b6afb5d31698d7df29ac7c8a39f9a1e5
Valentin Valentinov <95280488+vvalentinov@users.noreply.github.com>
2025-03-27 09:24:05 +0200

    Add some logic to Program.cs file to manipulate the collection

--- DelegatesAndLinq/Program.cs
+++ DelegatesAndLinq/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DelegatesAndLinq;
+
+var db = new MovieDb();
+var movies = db.Movies;
+
+var moviesService = new MoviesService();
+
+// Extension Methods
+var pulpFictionMovie = movies.GetMovieByTitle("Pulp Fiction");
+if (pulpFictionMovie is not null)
+{
+    Console.WriteLine("Searched Movie:");
+    Console.WriteLine(pulpFictionMovie.Title);
+
+    // add actor to movie
+    moviesService.AddActorToMovie(
+        pulpFictionMovie,
+        new Actor() { Name = "Valentin" });
+
+    var actor = pulpFictionMovie.Actors.FirstOrDefault(x => x.Name == "Valentin");
+    Console.WriteLine(actor?.Name);
+}
+
+Console.WriteLine();
+
+Console.WriteLine("Christopher Nolan Movies: ");
+var moviesWithDirector = movies.GetMoviesByDirectorName("Christopher Nolan");
+moviesWithDirector.WriteMovieTitlesToConsole();
+
+Console.WriteLine();
+
+Console.WriteLine("Updated Movie Titles and Director Names: ");
+moviesService.AddStringToMovieTitle(movies, "Movie Title: ");
+movies.WriteMovieTitlesToConsole();
+moviesService.AddStringToDirectorName(movies, "Director: ");
+movies.WriteMovieDirectorNameToConsole();
+
+Console.WriteLine();
+Console.WriteLine("Movies With Rating Bigger Than 7.9");
+var moviesWithBiggerRating = moviesService.GetMoviesWithAboveRating(movies, 7.9);
+moviesWithBiggerRating.WriteMovieTitlesToConsole();
+
+moviesService.AddSuffixToTitles(movies, " Best Movie Ever!");
+movies.WriteMovieTitlesToConsole();
