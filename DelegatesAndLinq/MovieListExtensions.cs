864fcdb1a7cbf08023792e31171f9f068431140e
Valentin Valentinov <95280488+vvalentinov@users.noreply.github.com>
2025-03-27 09:20:34 +0200

    Add movie list extensions class

---
+++ DelegatesAndLinq/MovieListExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace DelegatesAndLinq;
+
+public static class MovieListExtensions
+{
+    public static Movie? GetMovieByTitle(this IList<Movie> movies, string title)
+        => movies.FirstOrDefault(movie => movie.Title == title);
+
+    public static IList<Movie> GetMoviesByDirectorName(
+        this IList<Movie> movies,
+        string directorName)
+        => [.. movies.Where(movie => movie.Director.Name == directorName)];
+
+    public static void WriteMovieTitlesToConsole(this IList<Movie> movies)
+    {
+        foreach (var movie in movies)
+        {
+            Console.WriteLine(movie.Title);
+        }
+    }
+
+    public static void WriteMovieDirectorNameToConsole(this IList<Movie> movies)
+    {
+        foreach (var movie in movies)
+        {
+            Console.WriteLine(movie.Director.Name);
+        }
+    }
+}
