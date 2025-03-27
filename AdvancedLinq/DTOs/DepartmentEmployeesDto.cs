3d2f9e654305a637f8c6bd6d8468bdd99d3aa413
Valentin Valentinov <95280488+vvalentinov@users.noreply.github.com>
2025-03-27 09:59:19 +0200

    Create two DTOs - DepartmentEmployees and EmployeeDepartment

---
+++ AdvancedLinq/DTOs/DepartmentEmployeesDto.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace AdvancedLinq.DTOs;
+
+public class DepartmentEmployeesDto
+{
+    public string DepartmentName { get; set; } = string.Empty;
+
+    public List<string> EmployeesNames { get; set; } = [];
+
+    public override string ToString()
+    {
+        var builder = new StringBuilder();
+
+        builder.AppendLine(DepartmentName);
+
+        foreach (var employeeName in EmployeesNames)
+        {
+            builder.AppendLine(employeeName);
+        }
+
+        return builder.ToString();
+    }
+}
---
+++ AdvancedLinq/DTOs/EmployeeDepartmentDto.cs
@@ -0,0 +1,15 @@
+﻿namespace AdvancedLinq.DTOs;
+
+using static System.Environment;
+
+public class EmployeeDepartmentDto
+{
+    public int EmployeeId { get; set; }
+
+    public string EmployeeName { get; set; } = string.Empty;
+
+    public string DepartmentName { get; set; } = string.Empty;
+
+    public override string ToString()
+        => $"{nameof(EmployeeId)}: {EmployeeId}{NewLine}{nameof(EmployeeName)}: {EmployeeName}{NewLine}{nameof(DepartmentName)}: {DepartmentName}";
+}
