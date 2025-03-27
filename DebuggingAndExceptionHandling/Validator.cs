1508f43d4b9df80abb15df460f158d2ef1f270db
Valentin Valentinov <95280488+vvalentinov@users.noreply.github.com>
2025-03-27 08:43:13 +0200

    Add Validator Class

---
+++ DebuggingAndExceptionHandling/Validator.cs
@@ -0,0 +1,38 @@
+﻿namespace DebuggingAndExceptionHandling;
+
+public class Validator
+{
+    public static void AgeValidator(int age)
+    {
+        if (age <= 0)
+        {
+            throw new ArgumentException("Age cannot be less than or equal to zero!");
+        }
+
+        if (age > 120)
+        {
+            throw new ArgumentException("Age cannot be more than 120!");
+        }
+    }
+
+    public static void NameValidator(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            throw new ArgumentException("Name cannot ne null or empty!");
+        }
+
+        if (name.Length < 2 || name.Length > 750)
+        {
+            throw new ArgumentException("Name length must be between 2 and 750 characters long!");
+        }
+    }
+
+    public static void BalanceValidator(decimal balance)
+    {
+        if (balance < 0)
+        {
+            throw new NegativeBalanceException("Balance cannot be negative!");
+        }
+    }
+}
