c48c84c93112e9f3505460ab1fe64b7b2524f96a
Valentin Valentinov <95280488+vvalentinov@users.noreply.github.com>
2025-03-31 08:40:32 +0300

    Add disposal and garbage collection assignment

---
+++ src/Csharp&.NET Fundamentals/10. DisposalAndGarbageCollection/EmailService.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace _10._DisposalAndGarbageCollection;
+
+public class EmailService : IDisposable
+{
+    private readonly SmtpClient _smtpClient;
+    private bool _isDisposed;
+
+    public EmailService(string smtp, string username, string password)
+    {
+        _smtpClient = new SmtpClient(smtp)
+        {
+            Port = 587,
+            Credentials = new NetworkCredential(username, password),
+            EnableSsl = true,
+        };
+    }
+
+    public void SendEmail(
+        string fromEmailAddress,
+        string toEmailAddress,
+        string subject,
+        string body)
+    {
+        var mailMessage = new MailMessage
+        {
+            From = new MailAddress(fromEmailAddress),
+            Subject = subject,
+            Body = $"<h1>{body}</h1>",
+            IsBodyHtml = true,
+        };
+
+        mailMessage.To.Add(toEmailAddress);
+        _smtpClient.Send(mailMessage);
+    }
+
+    public void Dispose()
+    {
+        if (_isDisposed == false)
+        {
+            _smtpClient?.Dispose();
+            _isDisposed = true;
+        }
+    }
+
+}
---
+++ src/Csharp&.NET Fundamentals/10. DisposalAndGarbageCollection/Program.cs
@@ -0,0 +1,34 @@
+﻿// Get Secrets
+using _10._DisposalAndGarbageCollection;
+using Microsoft.Extensions.Configuration;
+
+var configurationBuilder = new ConfigurationBuilder();
+
+IConfiguration configuration = configurationBuilder
+    .AddUserSecrets<Program>()
+    .Build();
+
+string smtp = configuration["smtp"];
+string fromEmail = configuration["fromEmailAddress"];
+string username = configuration["username"];
+string password = configuration["password"];
+
+var emailService = new EmailService(smtp ,username, password);
+
+Console.WriteLine("Subscribe to the newsletter by providing your email below:");
+Console.Write("Email: ");
+
+string toEmail = Console.ReadLine() ?? string.Empty;
+while (string.IsNullOrWhiteSpace(toEmail))
+{
+    Console.WriteLine("Error! You must provide an email address!");
+    Console.Write("Email: ");
+    toEmail = Console.ReadLine() ?? string.Empty;
+}
+
+emailService.SendEmail(
+    fromEmail,
+    toEmail,
+    "subject",
+    "body"
+);
